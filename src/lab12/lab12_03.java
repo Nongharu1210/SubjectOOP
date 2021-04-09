1e6faffee77321d70a4b8010ab5feb1f4a667b94
Nongharu1210 <6306021422194@fitm.kmutnb.ac.th>
2021-04-09 15:58:17 +0700

    update

--- src/lab12/lab12_03.java
+++ src/lab12/lab12_03.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 public class lab12_03 extends JFrame implements ActionListener, MouseListener {
     int x1, y1, x2, y2;
-    JButton btnLine, btnRect, btnCircle, btnClear, redBtn, greenBtn, blueBtn;
+    JButton btnLine, btnRect, btnCircle, btnClear, blackBtn, blueBtn, cyanBtn, dark_grayBtn, grayBtn, greenBtn, lightBtn, magentaBtn, yellowBtn, pinkBtn, redBtn, whiteBtn;
     JTextField text1, text2;
     Color oldColor1, oldColor2;
     char typeShape = 'L';
@@ -15,30 +15,121 @@ public class lab12_03 extends JFrame implements ActionListener, MouseListener {
         super("");
         Container c = getContentPane();
         c.setLayout(new FlowLayout());
-        btnLine = new JButton("Line");
+        btnLine = new JButton("     Line     ");
         btnLine.addActionListener(this);
+        btnLine.setBackground(Color.YELLOW);
+        btnLine.setForeground(Color.RED);
+        btnLine.setForeground(Color.RED);
+        btnLine.setBorder(BorderFactory.createLineBorder(Color.RED));
         c.add(btnLine);
-        btnRect = new JButton("Rectangle");
+
+        btnRect = new JButton("     Rectangle     ");
         btnRect.addActionListener(this);
+        btnRect.setBackground(Color.YELLOW);
+        btnRect.setForeground(Color.RED);
+        btnRect.setForeground(Color.RED);
+        btnRect.setBorder(BorderFactory.createLineBorder(Color.RED));
         c.add(btnRect);
-        btnCircle = new JButton("Circle");
+
+        btnCircle = new JButton("     Circle     ");
         btnCircle.addActionListener(this);
+        btnCircle.setBackground(Color.YELLOW);
+        btnCircle.setForeground(Color.RED);
+        btnCircle.setForeground(Color.RED);
+        btnCircle.setBorder(BorderFactory.createLineBorder(Color.RED));
         c.add(btnCircle);
-        btnClear = new JButton("Clear");
+        
+        btnClear = new JButton("     Clear     ");
         btnClear.addActionListener(this);
+        btnClear.setBackground(Color.YELLOW);
+        btnClear.setForeground(Color.RED);
+        btnClear.setForeground(Color.RED);
+        btnClear.setBorder(BorderFactory.createLineBorder(Color.RED));
         c.add(btnClear);
         //************************************//
-        redBtn = new JButton("Red");
-        redBtn.addActionListener(this);
-        c.add(redBtn);
+        blackBtn = new JButton("      ");
+        blackBtn.addActionListener(this);
+        blackBtn.setBackground(Color.black);
+        blackBtn.setForeground(Color.white);
+        blackBtn.setBorder(BorderFactory.createLineBorder(Color.black));
+        c.add(blackBtn);
 
-        greenBtn = new JButton("Green");
+        blueBtn = new JButton("      ");
+        blueBtn.addActionListener(this);
+        blueBtn.setBackground(Color.blue);
+        blueBtn.setForeground(Color.white);
+        blueBtn.setBorder(BorderFactory.createLineBorder(Color.black));
+        c.add(blueBtn);
+
+        cyanBtn = new JButton("      ");
+        cyanBtn.addActionListener(this);
+        cyanBtn.setBackground(Color.cyan);
+        cyanBtn.setForeground(Color.white);
+        cyanBtn.setBorder(BorderFactory.createLineBorder(Color.black));
+        c.add(cyanBtn);
+
+        dark_grayBtn = new JButton("      ");
+        dark_grayBtn.addActionListener(this);
+        dark_grayBtn.setBackground(Color.DARK_GRAY);
+        dark_grayBtn.setForeground(Color.white);
+        dark_grayBtn.setBorder(BorderFactory.createLineBorder(Color.black));
+        c.add(dark_grayBtn);
+
+        grayBtn = new JButton("      ");
+        grayBtn.addActionListener(this);
+        grayBtn.setBackground(Color.GRAY);
+        grayBtn.setForeground(Color.white);
+        grayBtn.setBorder(BorderFactory.createLineBorder(Color.black));
+        c.add(grayBtn);
+
+        greenBtn = new JButton("      ");
         greenBtn.addActionListener(this);
+        greenBtn.setBackground(Color.GREEN);
+        greenBtn.setForeground(Color.white);
+        greenBtn.setBorder(BorderFactory.createLineBorder(Color.black));
         c.add(greenBtn);
 
-        blueBtn = new JButton("Blue");
-        blueBtn.addActionListener(this);
-        c.add(blueBtn);
+        lightBtn = new JButton("      ");
+        lightBtn.addActionListener(this);
+        lightBtn.setBackground(Color.LIGHT_GRAY);
+        lightBtn.setForeground(Color.white);
+        lightBtn.setBorder(BorderFactory.createLineBorder(Color.black));
+        c.add(lightBtn);
+
+        magentaBtn = new JButton("      ");
+        magentaBtn.addActionListener(this);
+        magentaBtn.setBackground(Color.MAGENTA);
+        magentaBtn.setForeground(Color.white);
+        magentaBtn.setBorder(BorderFactory.createLineBorder(Color.black));
+        c.add(magentaBtn);
+
+        yellowBtn = new JButton("      ");
+        yellowBtn.addActionListener(this);
+        yellowBtn.setBackground(Color.YELLOW);
+        yellowBtn.setForeground(Color.white);
+        yellowBtn.setBorder(BorderFactory.createLineBorder(Color.black));
+        c.add(yellowBtn);
+
+        pinkBtn = new JButton("      ");
+        pinkBtn.addActionListener(this);
+        pinkBtn.setBackground(Color.PINK);
+        pinkBtn.setForeground(Color.white);
+        pinkBtn.setBorder(BorderFactory.createLineBorder(Color.black));
+        c.add(pinkBtn);
+
+        redBtn = new JButton("      ");
+        redBtn.addActionListener(this);
+        redBtn.setBackground(Color.RED);
+        redBtn.setForeground(Color.white);
+        redBtn.setBorder(BorderFactory.createLineBorder(Color.black));
+        c.add(redBtn);
+
+        whiteBtn = new JButton("      ");
+        whiteBtn.addActionListener(this);
+        whiteBtn.setBackground(Color.WHITE);
+        whiteBtn.setForeground(Color.white);
+        whiteBtn.setBorder(BorderFactory.createLineBorder(Color.black));
+        c.add(whiteBtn);
         //************************************//
         text1 = new JTextField(12);
         text1.setEditable(false);
@@ -47,6 +138,7 @@ public class lab12_03 extends JFrame implements ActionListener, MouseListener {
         text2.setEditable(false);
         c.add(text2);
         addMouseListener(this);
+        
     }
 
     public void paint(Graphics g) {
